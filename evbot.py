16811ed67c6dffdd1349a6fcbdf46b5d751a8aba
William Tichenor <williamtichenor@tamu.edu>
2024-12-27 22:32:50 -0600

    longe query

--- evbot.py
+++ evbot.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
             if querybow[1]<=5:
                 factor = (1/20)*querybow[1]+0.8
             else:
-                factor = 4/(querybow[1])
+                factor = 20/(querybow[1]+16)
             scores[doc[0]] = (random.random()/2.5+0.8) * score * factor
         scores = dict(sorted(scores.items(), key=lambda item: item[1], reverse=True))
         print(dict(list(scores.items())[:10]))
