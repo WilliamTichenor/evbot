17b0c6b50979dfca8571b5589349409e08d08fb3
William Tichenor <williamtichenor@tamu.edu>
2024-12-27 22:40:18 -0600

    lobot

--- evbot.py
+++ evbot.py
@@ -106,7 +106,7 @@ if __name__ == "__main__":
         scores = dict(sorted(scores.items(), key=lambda item: item[1], reverse=True))
         print(dict(list(scores.items())[:10]))
 
-        if len(scores) > 0 and scores[list(scores)[0]] > 19.0:
+        if len(scores) > 0 and scores[list(scores)[0]] > 24.0:
             await message.channel.send(list(scores)[0])
 
 
